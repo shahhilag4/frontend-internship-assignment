b1c028c8e5d503356384d4ee7dca3fc727f12527
shahhilag4 <shahhilag16@gmail.com>
2023-04-20 21:00:00 +0530

    Solved warnings

--- src/app/components/trending-subjects/trending-subjects.component.spec.ts
+++ src/app/components/trending-subjects/trending-subjects.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TrendingSubjectsComponent } from './trending-subjects.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('TrendingSubjectsComponent', () => {
