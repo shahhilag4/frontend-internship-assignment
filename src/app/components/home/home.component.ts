8a4f7d8cbec4d3642d780c160f794be8308991e0
shahhilag4 <shahhilag16@gmail.com>
2023-04-24 22:35:02 +0530

    Base

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { debounceTime, filter, switchMap } from 'rxjs/operators';
 import { BookService } from '../../core/services/book.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { PageEvent } from '@angular/material/paginator';
 import { HttpClient } from '@angular/common/http';
-
-
 @Component({
   selector: 'front-end-internship-assignment-home',
   templateUrl: './home.component.html',
@@ -15,22 +13,19 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   // Variable declaration
   bookSearch: FormControl;
-  books: Array<any> = []; 
+  books: Array<any> = [];
   showNoResultsMessage = false;
   loading = false;
-  
-  //Pagination variables
   pageIndex = 1;
   totalResults = 0;
-  currentPage=1;
-  pageSize =10;
+  //Pagination variables
+  currentPage = 1;
+  pageSize = 10;
   cachedBooks: { [key: string]: Array<any> } = {};
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  constructor(private bookService: BookService,private http: HttpClient) {
+  constructor(private bookService: BookService, private http: HttpClient) {
     this.bookSearch = new FormControl('');
   }
-  
 
   trendingSubjects: Array<any> = [
     { name: 'JavaScript' },
@@ -41,8 +36,14 @@ export class HomeComponent implements OnInit {
   ];
 
   //Load functionalities on starting of the application -----> Debouncing functionality leveraged
-  ngOnInit(): void {
-          this.onSearch(); // call search functionality to get accurate page count     
+  ngOnInit() {
+    this.bookSearch.valueChanges
+      .pipe(
+        debounceTime(300) // wait for 500ms before emitting the next value
+      )
+      .subscribe((query) => {
+        this.onSearch();
+      });
   }
   //reset paginator data on new query detection
   resetPaginator(): void {
@@ -53,84 +54,52 @@ export class HomeComponent implements OnInit {
   }
   //Search functionality
   onSearch(): void {
-    this.bookSearch.valueChanges.pipe(
-      debounceTime(300),
-      filter((value: string) => value.length > 0)
-    ).subscribe((searchKey: string) => {
-      if (this.loading) {
-        return; // don't trigger search if already loading
-      }
-      if (searchKey.trim().length < 1) {
-        this.clearTable();
-        return;
-      }
-      this.loading = true; // set loading to true
-      
-      //Caching
-      const cacheKey = searchKey.trim().toLowerCase(); // normalize cache key
-      if (this.cachedBooks[cacheKey]) { // check if data is already cached
+    const query: string = this.bookSearch.value;
+    if (query.trim().length < 3) {
+      this.clearTable();
+      return;
+    }
+    this.loading = true; // set loading to true
+    this.bookService
+      .searchBooks(query, this.pageIndex, this.pageSize)
+      .subscribe((response: any) => {
         this.loading = false; // set loading to false
-        this.books = this.cachedBooks[cacheKey]; // get data from cache
-        this.resetPaginator(); // reset the paginator to the first page after the search results are loaded
+        this.books = response.docs.map((book: any) => {
+          return {
+            title: book.title,
+            authorName: book.author_name ? book.author_name.join(', ') : '',
+            year: book.first_publish_year || '',
+            isbn: book.isbn ? book.isbn[0] : '',
+            bookUrl: `https://openlibrary.org/books/${
+              book.cover_edition_key ? book.cover_edition_key : book.key
+            }`,
+          };
+        });
         // show "No results found" if search results are empty
         if (this.books.length === 0) {
           this.showNoResultsMessage = true;
         } else {
           this.showNoResultsMessage = false;
         }
-        return;
-      }
-      //Fetching data from API
-      this.bookService.searchBooks(searchKey, this.currentPage, this.pageSize)
-        .subscribe((data: any) => {
-          this.loading = false; // set loading to false
-          this.books = data.docs.map((book: any) => {
-            return {
-              title: book.title,
-              authorName: book.author_name ? book.author_name.join(', ') : '',
-              year: book.first_publish_year || '',
-              isbn: book.isbn ? book.isbn[0] : '',
-              bookUrl: `https://openlibrary.org/books/${book.cover_edition_key ? book.cover_edition_key : book.key}`
-            };
-          });
-          this.onSearch(); // call search functionality to get accurate page count
-          if (this.books.length === 0) {
-            this.showNoResultsMessage = true;
-          } else {
-            this.showNoResultsMessage = false;
-          }
-          if((<HTMLInputElement>document.getElementById("search")).value.length==0)
-          {
-            this.clearTable();
-          }
-  
-          // reset the paginator to the first page after the search results are loaded
-          if (this.paginator) {
-            this.paginator.firstPage();
-            this.paginator.length = this.books.length;
-          }
-          this.cachedBooks[cacheKey] = this.books; // add data to cache
-        });
-    });
-  
-    this.bookSearch.valueChanges.subscribe((value: string) => {
-      if (value.trim().length == 0) {
-        this.clearTable();
-      }
-    });
+        this.totalResults = response.numFound; // update totalResults property
+        console.log(this.totalResults);
+        this.pageCount(); // call pageCount to update paginator length
+        this.paginator.firstPage(); // reset paginator to first page
+      });
   }
   //Count the number of pages based on a formula
   pageCount(): number {
     const count = Math.ceil(this.totalResults / this.pageSize); // calculate number of pages
     return count;
   }
-  
+
   //On clicking next or previous this function will be called
   onPageChange(event: PageEvent) {
     this.pageIndex = event.pageIndex + 1; // update pageIndex property
     this.pageSize = event.pageSize; // update pageSize property
     const query: string = this.bookSearch.value;
-    this.bookService.searchBooks(query, this.pageIndex, this.pageSize)
+    this.bookService
+      .searchBooks(query, this.pageIndex, this.pageSize)
       .subscribe((response: any) => {
         this.loading = false;
         this.books = response.docs.map((book: any) => {
@@ -139,7 +108,9 @@ export class HomeComponent implements OnInit {
             authorName: book.author_name ? book.author_name.join(', ') : '',
             year: book.first_publish_year || '',
             isbn: book.isbn ? book.isbn[0] : '',
-            bookUrl: `https://openlibrary.org/books/${book.cover_edition_key ? book.cover_edition_key : book.key}`
+            bookUrl: `https://openlibrary.org/books/${
+              book.cover_edition_key ? book.cover_edition_key : book.key
+            }`,
           };
         });
         this.totalResults = response.numFound; // update totalResults property
@@ -150,9 +121,8 @@ export class HomeComponent implements OnInit {
         }
       });
   }
-  
-  
-  //clear all books when the query is cleared  
+
+  //clear all books when the query is cleared
   clearTable(): void {
     this.books = [];
     this.showNoResultsMessage = false;
@@ -160,5 +130,5 @@ export class HomeComponent implements OnInit {
   //clear searches on clicking button
   clearSearch(): void {
     this.bookSearch.setValue('');
-  } 
+  }
 }
--- src/app/core/services/book.service.ts
+++ src/app/core/services/book.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,19 @@ export class BookService {
   //Setting a base URL to search and get data
   private baseUrl = 'https://openlibrary.org';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  //Basic functionality to search books
-  searchBooks(searchKey: string, pageIndex: number, pageSize: number) {
-    const offset = (pageIndex - 1) * pageSize;
-    const searchUrl = `${this.baseUrl}/search.json?q=${searchKey}&fields=title,author_name,first_publish_year,book_url,first_publish_year,isbn,cover_edition_key&limit=${pageSize}&offset=${offset}`;
-    return this.http.get(searchUrl);
+  //Modified functionality to search books using RxJS caching
+  searchBooks(searchKey: string, pageIndex: number, pageSize: number): Observable<any> {
+    const searchUrl = `${this.baseUrl}/search.json?q=${searchKey}&fields=title,author_name,first_publish_year,book_url,first_publish_year,isbn,cover_edition_key&page=${pageIndex}&limit=10`;
+    return this.http.get(searchUrl).pipe(shareReplay());
   }
-  
+
+  //Modified functionality to get books using RxJS caching
+  getBooks(page: number, pageSize: number): Observable<any> {
+    const offset = (page - 1) * pageSize;
+    const limit = pageSize;
+    const searchUrl = `${this.baseUrl}/search.json?q=javascript&fields=title,author_name,first_publish_year,book_url,first_publish_year,isbn,cover_edition_key&offset=${offset}&limit=10`;
+    return this.http.get(searchUrl).pipe(shareReplay());
+  }
 }
